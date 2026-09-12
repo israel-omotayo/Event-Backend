e05c5eff81ec0d7a2b2b4a5dd16833a10283591c
Israel Omotayo <omotayoisrael24@gmail.com>
2026-09-12 19:14:34 +0100

    Added health endpoint for ping

--- reg_system/urls.py
+++ reg_system/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .health import health_check
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.permissions import AllowAny
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(permission_classes=[AllowAny]), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema', permission_classes=[AllowAny]), name='swagger-ui'),
     path('', include('accounts.urls')),
